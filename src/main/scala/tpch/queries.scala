4c867ffba8a8cefbaf6f4b25087d6be223b9f73c
Stephen Tu <stephentu@csail.mit.edu>
2012-06-05 13:26:40 -0400

    fix q11 to spec

--- src/main/scala/tpch/queries.scala
+++ src/main/scala/tpch/queries.scala
@@ -326,7 +326,7 @@ from
 where
   ps_suppkey = s_suppkey
   and s_nationkey = n_nationkey
-  and n_name = 'nnation'
+  and n_name = 'ARGENTINA'
 group by
   ps_partkey having
     sum(ps_supplycost * ps_availqty) > (
